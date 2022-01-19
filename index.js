21db565633e1e7a48566e90830f7768500222bec
alkcode <alek_1604@comunidad.unam.mx>
2022-01-18 20:52:33 -0600

    Commit

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/api/auth', require('./routes/auth'));
 
 // manejar posibles rutas
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 
